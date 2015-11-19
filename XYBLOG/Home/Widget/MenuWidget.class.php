bd38936c2838f6fc1ef661082e27b64a38ad941b
dingdayu <614422099@qq.com>
2015-11-19 17:39:30 +0800

    修复列表显示

--- XYBLOG/Home/Widget/MenuWidget.class.php
+++ XYBLOG/Home/Widget/MenuWidget.class.php
@@ -16,7 +16,7 @@ class MenuWidget extends Controller{
 
     /* 显示指定分类的同级分类或子分类列表 */
     public function header(){
-        $list[] = array('url'=>U('Blog/index'),'title'=>'博客列表');
+        $list[] = array('url'=>U('Home/Blog/index'),'title'=>'博客列表');
 
         $this->assign('list', $list);
         $this->display('Widget/menu_header');
