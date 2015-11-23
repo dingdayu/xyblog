4381c55771436c722c290c595d7c493177c9238e
dingdayu <614422099@qq.com>
2015-11-23 15:24:28 +0800

    提交Admin分组
    Signed-off-by: dingdayu <614422099@qq.com>

--- XYBLOG/Admin/Conf/config.php
+++ XYBLOG/Admin/Conf/config.php
@@ -5,4 +5,9 @@ return array(
     'TMPL_PARSE_STRING'  =>array(
         '__Public__' => '/XYBLOG/Home/View/default/public',
     ),
+    'LOAD_EXT_CONFIG'   =>  'user',
+
+
+    // 设置默认的模板主题
+    'DEFAULT_THEME'    =>    'MaterialAdmin',
 );
--- XYBLOG/Admin/Conf/user.php
+++ XYBLOG/Admin/Conf/user.php
@@ -10,4 +10,10 @@ return array(
     'USER_TYPE'      => 2, //用户类型
     'USER_AUTH_ID'   => 10, //用户认证ID
     'USER_AUTH_TYPE' => 2, //用户认证模式
+
+
+    //登录认证设置
+    'AUTH_CONFIG'       =>  array(
+        'AUTH_USER'     =>  'user',
+    ),
 );
